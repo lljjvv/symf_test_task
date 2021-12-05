b03451b63f7c62783fef35ca5d670171e0b3b8bc
Josan Liviu <l.josan@unifun.com>
2021-12-05 21:18:47 +0200

    Added data parser in job controller

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Job;
+use GuzzleHttp\Client;
+use Symfony\Component\CssSelector;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -21,7 +24,7 @@ class JobController extends AbstractController
         ]);
     }
 
-        /**
+    /**
      * @Route("/job/create", name="create_job")
      */
     public function createJob(ManagerRegistry $doctrine, ValidatorInterface $validator): Response
@@ -43,5 +46,44 @@ class JobController extends AbstractController
         $entityManager->flush();
 
         return new Response('Saved new job with id ' . $job->getId());
+    }
+
+    /**
+     * @Route("/job/crawl/{url}", name="crawl_jobs")
+     */
+    public function crawlJobs(string $url = 'https://www.bestjobs.eu/ro/locuri-de-munca?location=bucuresti&keyword=symfony', ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    {
+
+        $client = new Client();
+        $response = $client->request('GET', $url);
+        $body = (string) $response->getBody(true);
+
+        $crawler = new Crawler($body);
+
+        $jobs = $crawler->filter('.list-card')
+            ->each(function (Crawler $node, $i) {
+
+                $job_ent = new Job();
+                $job_ent->setTitle($node->filter('h3 > small')->text());
+                $job_ent->setCompany($node->filter('h2 > strong')->text());
+                $job_ent->setLocation($node->filter('span > a')->text());
+                $job_ent->setDescription("");
+
+                return $job_ent;
+            });
+
+        $entityManager = $doctrine->getManager();
+        foreach ($jobs as $job) {
+
+            $errors = $validator->validate($job);
+            if (count($errors) > 0) {
+                return false;
+            }
+
+            $entityManager->persist($job);
+            $entityManager->flush();
+        }
+
+        return new Response('</br>Parsed data from ' . $url . "</br><pre>" . json_encode($jobs) . "</pre>");
     }
 }
